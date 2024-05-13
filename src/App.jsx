a52b7e58ab3214a22d99c19df2332f32f51e5f9a
kiran-930 <kiranperiye@gmail.com>
2024-05-13 23:32:37 +0530

    Task completed successfully

---
+++ src/App.jsx
@@ -0,0 +1,175 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+
+      <nav>
+        <div className="nav__logo"><a href="#">Captured Imagery</a></div>
+        <ul className="nav__links">
+          <li className="link" href="#home" target="_blank"><a href="#home">Home</a></li>
+          <li className="link" href="#blog" target="_blank"><a href="#gallery">Gallery</a></li>
+          <li className="link" href="#service" target="_blank"><a href="#service">Services</a></li>
+          <li className="link" href="#contact" target="_blank"><a href="#contacts">Contacts</a></li>
+        </ul>
+      </nav>
+      <section id='home'>
+      <header>
+        <div className="section__container">
+          <div className="header__content">
+            <h1>Photography</h1>
+            <p>
+              Most famous Photographs in history <br />
+              "There are two people in every picture: the phographer and the viewer"
+            </p>
+            <button>Read more</button>
+          </div>
+        </div>
+      </header>
+</section>
+
+    
+  <section id="gallery"></section>
+  <div class="package" id="pac">
+          <h1>World Best Photpgraphs</h1>
+          <div class="pack-1 p1">
+            <div class="bigP">
+              <div class="pac">
+                <p>Hidden smile</p> <br />
+                <p>by famous photographer rehahn</p>
+    
+    
+                <div class="star">
+                  <i class="fa-solid fa-star fas"></i>
+                  <i class="fa-solid fa-star fas"></i>
+                  <i class="fa-solid fa-star fas"></i>
+                  <i class="fa-solid fa-star fas"></i>
+                  <i class="fa-solid fa-star fas"></i>
+                </div>
+               
+                <div class="dbox">
+                 
+                </div>
+              </div>
+            </div>
+          </div>
+    
+          <div class="pack-1 p2">
+            <div class="bigP">
+              <div class="pac">
+                <p>Tribal</p> <br />
+                <p>by famous photographer jimmy nelson</p>
+    
+                <div class="star">
+                  <i class="fa-solid fa-star fas"></i>
+                  <i class="fa-solid fa-star fas"></i>
+                  <i class="fa-solid fa-star fas"></i>
+                  <i class="fa-solid fa-star fas"></i>
+                  <i class="fa-regular fa-star ff"></i>
+                </div>
+              
+                <div class="dbox">
+                 
+                </div>
+              </div>
+            </div>
+          </div>
+    
+          <div class="pack-1 p3">
+            <div class="bigP">
+              <div class="pac">
+                <p>The Afgham Girl</p> <br />
+                <p>by famous photographer Steve McCurry</p>
+    
+                <div class="star">
+                  <i class="fa-solid fa-star fas"></i>
+                  <i class="fa-solid fa-star fas"></i>
+                  <i class="fa-solid fa-star fas"></i>
+                  <i class="fa-solid fa-star fas"></i>
+                  <i class="fa-star fa-regular ff"></i>
+                </div>
+                
+                <div class="dbox">
+                
+                </div>
+              </div>
+            </div>
+          </div>
+    
+          <div class="pack-1 p4">
+            <div class="bigP">
+              <div class="pac">
+                <p>Portrait</p> <br />
+                <p>by famous photographer Lee Jeffries</p>
+    
+                <div class="star">
+                  <i class="fa-solid fa-star fas"></i>
+                  <i class="fa-solid fa-star fas"></i>
+                  <i class="fa-solid fa-star fas"></i>
+                  <i class="fa-solid fa-star fas"></i>
+                  <i class="fa-solid fa-star fas"></i>
+                </div>
+                
+                <div class="dbox">
+                 
+                </div>
+              </div>
+            </div>
+          </div>
+    
+          <div class="pack-1 p5">
+            <div class="bigP">
+              <div class="pac">
+                <p>Tribal</p>
+                <p>by famous photographer jimmy nelson</p>
+    
+                <div class="star">
+                  <i class="fa-solid fa-star fas"></i>
+                  <i class="fa-solid fa-star fas"></i>
+                  <i class="fa-solid fa-star fas"></i>
+                  <i class="fa-solid fa-star fas"></i>
+                  <i class="fa-regular fa-star ff"></i>
+                </div>
+            
+                <div class="dbox">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+  
+
+      <footer class="footer">
+      <div class="section__container footer__container">
+        <ul class="footer__socials">
+          <li>
+            <a href="#"><i class="ri-facebook-fill"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="ri-twitter-fill"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="ri-pinterest-line"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="ri-linkedin-fill"></i></a>
+          </li>
+        </ul>
+        <div class="footer__bar">
+        Captured Imagery
+      </div>
+      </div>
+     
+    </footer>
+      
+    </>
+  )
+}
+
+export default App
